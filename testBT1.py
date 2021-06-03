fb052e6b98a5fdc658ab3ee76e5ee91946de38ff
100ask <weidongshan@qq.com>
2021-06-03 13:32:49 +0000

    bt client and server ok

--- testBT1.py
+++ testBT1.py
@@ -1,4 +1,4 @@
-import android, time, sys
+import android, time, sys,os
 import json
 
 
@@ -10,50 +10,40 @@ locName = droid.bluetoothGetLocalName()
 print(locAddr)
 print(locName)
 
-rAddr = '38:E6:0A:3F:4F:22'
-rName = droid.bluetoothGetRemoteDeviceName(rAddr)
-print(rName)
+rAddr = '38:E6:0A:3F:4F:22'#redmi5
+#rAddr = '50:98:39:a9:5a:19'#redmi9
+#rAddr = '88:BF:E4:C1:14:FC'#HUAWEI
+#rName = droid.bluetoothGetRemoteDeviceName(rAddr)
 
 #scanMode = droid.bluetoothGetScanMode()
 #turn on bluetooth
 droid.toggleBluetoothState(True)
 
-#ask user
 
-'''
-droid.dialogCreateAlert('Be a server?')
-droid.dialogSetPositiveButtonText('Yes')
-droid.dialogSetNegativeButtonText('No')
-droid.dialogShow()
-
-#get user response to question
-result = droid.dialogGetResponse()
-
-#if the result is 'Yes' ('positive') then is_server is set to True
-is_server = result.result['which'] == 'positive'
-'''
 counter = 0
-uuid = '00001101-0000-1000-8000-00805F9B34FB'
-#uuid = '457807c0-4897-11df-9879-0800200c9a66'
+#uuid = '00001101-0000-1000-8000-00805F9B34FB'
+uuid = '457807c0-4897-11df-9879-0800200c9a66'
 connID = ''
-while counter < 10:
+while counter < 100:
    
   time.sleep(1) 
   print("connecting")
-  rvl = droid.bluetoothConnect('00001101-0000-1000-8000-00805F9B34FB','38:E6:0A:3F:4F:22')
+  rvl = droid.bluetoothConnect(uuid,rAddr)
   print(rvl)
   #rvl = droid.bluetoothConnect('00001101-0000-1000-8000-00805F9B34FB')
   if rvl.error == None:
      connID = rvl.result
      connets = droid.bluetoothActiveConnections()
+     print('connID='+connID)
      print(connets)
-     name = droid.bluetoothGetConnectedDeviceName(connID)
-     print(name)
      break
         
   counter = counter+1
- 
-time.sleep(10) 
+if counter>99:
+    droid.exit()
+    os.exit(0)
+    
+time.sleep(1) 
 #https://wenku.baidu.com/view/59c2b2849e31433238689323.html
 
 
@@ -63,9 +53,9 @@ time.sleep(10)
 
 while True: #receives a message
   time.sleep(1)
-  droid.bluetoothWrite('sending' + '\n',connID) #otherwise write the message
-  print("waiting")
-  #message = droid.bluetoothReadLine(connID).result
-  #print(message)  
+  print('send and waiting')
+  droid.bluetoothWrite('Redmi9 send to Server' + '\n',connID) #otherwise write the message
+  message = droid.bluetoothReadLine(connID).result
+  print('Rcv Msg :'+message)  
 
 droid.exit()
